9fbda1fb833c1a5ce99bc6440eb7b399663485d2
deepanshu <deepanshu6sharawat@gmail.com>
2026-08-02 22:07:13 +0530

    abc

---
+++ 2.2 Native Modules/index.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+// fs.writeFile("abc.txt", "hello from node.js !!!! yeeeeeeeeeee", (err) => {
+//     if(err) throw err;
+//     console.log("file saved successfully ")
+// });
+
+// import { readFile } from 'node:fs';
+
+fs.readFile('./message.txt', "utf8",(err, data) => {
+  if (err) throw err;
+  else {
+  console.log(data);
+  console.log("work done successfully !")
+  }
+});
